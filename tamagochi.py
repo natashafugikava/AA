9786b6af8bf5ed7566fbe1d45c26d4ad9714401f
natashafugikava <113446005+natashafugikava@users.noreply.github.com>
2023-10-04 19:59:30 -0300

    Create tamagochi.py

---
+++ tamagochi.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+class Tamagochi:
+  def __init__(self,nome):
+    self.nome = nome
+    self.fome = 100
+    self.saude = 100
+    self.idade = 0
+
+  def set_nome(self, nome):
+    self.nome = nome
+
+  def set_fome(self, fome):
+    self.fome = fome
+
+  def set_saude(self, saude):
+    self.saude = saude
+
+  def set_idade(self, idade):
+    self.idade = idade
+
+st.title('Jogo do bichinho virtual da Natasha!')
